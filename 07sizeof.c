cc8c631673956de3b8e9b938d2f97ecce71f8f74
niravpatel22 <109520761+niravpatel22@users.noreply.github.com>
2022-08-12 11:09:44 +0530

    xyz

---
+++ 07sizeof.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+	char ch ='y';
+	
+	//printf("");
+	printf("%d",sizeof(ch));
+	//printf("Size of double datatype = %d bytes",sizeof(double));
+	
+	
+	
+	
+	
+	return 0;
+}
